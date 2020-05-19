bf8534742c56a1ba1fd392cb615c7179797d9e06
andrew <arenn@design.upenn.edu>
2020-05-18 20:56:12 -0400

    Exploratory

--- R/network.R
+++ R/network.R
@@ -55,6 +55,44 @@ hoods <-
   rename(fips = name) %>%
   left_join(counties_simplified)
 
+theme_bm_legend <- function () {
+  theme_void() + 
+    theme(plot.background = element_rect(fill = 'black', colour = 'black'),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          panel.grid.minor.y = element_blank(),
+          axis.line.x = element_blank(),
+          axis.line.y = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          legend.title = element_text(colour = 'grey50', angle = 270),
+          legend.text = element_text(colour = 'white', angle = 270),
+          plot.title = element_text(face = 'bold', colour = 'grey50'),
+          plot.subtitle =  element_text(face = 'plain', colour = 'white', size = 15),
+          panel.grid.major = element_line(size = NA), 
+          panel.grid.minor = element_line(size = NA),
+          legend.position = c(0.9, 0.25),
+          plot.margin = margin(20, 20, 20, 20)
+    )
+  
+}
+
+guide_discrete <-
+  guide_legend(direction = "vertical",
+               keyheight = unit(5, units = "mm"),
+               keywidth = unit(2, units = "mm"),
+               title.position = 'right',
+               label.position = 'left',
+               title.hjust = 0.5,
+               label.hjust = 1,
+               ncol = 1,
+               bycol = TRUE)
+
+library(RColorBrewer)
+
 hoods %>%
   activate(nodes) %>%
   as_tibble() %>%
@@ -62,5 +100,9 @@ hoods %>%
   ggplot(aes(fill = factor(group))) +
   geom_sf(colour = 'white', size = 0.1) +
   geom_sf(data = states_simplified, aes(), 
-          fill = NA, colour = 'white', size = 0.5)
+          fill = NA, colour = 'white', size = 0.5) +
+  scale_fill_brewer(palette = 'Paired', 
+                    name = 'group', 
+                    guide = guide_discrete) +
+  theme_bm_legend()
 
