f1bc0b670412016543b90cd6c166835764426b4a
andrew <arenn@design.upenn.edu>
2020-05-21 10:25:34 -0400

    Commit from RStudio

--- R/network.R
+++ R/network.R
@@ -3,6 +3,8 @@ library(data.table)
 library(dtplyr)
 library(janitor)
 
+##
+
 lex <- 
   read_csv("data/lex_data/county_lex_2020-05-11.csv.gz") %>%
   clean_names() %>%
@@ -10,10 +12,16 @@ lex <-
   filter(value > 0) %>%
   mutate(fips = str_remove_all(fips, "x"))
 
+##
+
 library(sf)
 library(rmapshaper)
 
+##
+
 counties <- read_sf("https://github.com/asrenninger/exposure/raw/master/data/map_data/projected-geography.geojson")
+
+##
 
 counties_simplified <- ms_simplify(counties, 0.5)
 
@@ -21,6 +29,8 @@ states_simplified <-
   counties_simplified %>%
   group_by(state) %>%
   summarise()
+
+##
 
 verts <-
   lex %>%
@@ -36,8 +46,12 @@ links <-
          to = fips,
          weight = value)
 
+##
+
 library(igraph)
 library(tidygraph)
+
+##
 
 graph <- graph_from_data_frame(links, vertices = verts, directed = FALSE)
 
@@ -48,12 +62,16 @@ plot(graph,
 
 graph <- as_tbl_graph(graph)
 
+##
+
 hoods <- 
   graph %>%
   activate(nodes) %>%
   mutate(group = group_louvain()) %>%
   rename(fips = name) %>%
   left_join(counties_simplified)
+
+##
 
 theme_bm_legend <- function () {
   theme_void() + 
@@ -74,7 +92,7 @@ theme_bm_legend <- function () {
           plot.subtitle =  element_text(face = 'plain', colour = 'white', size = 15),
           panel.grid.major = element_line(size = NA), 
           panel.grid.minor = element_line(size = NA),
-          legend.position = c(0.9, 0.25),
+          legend.position = c(0.95, 0.25),
           plot.margin = margin(20, 20, 20, 20)
     )
   
@@ -91,7 +109,11 @@ guide_discrete <-
                ncol = 1,
                bycol = TRUE)
 
+##
+
 library(RColorBrewer)
+
+##
 
 hoods %>%
   activate(nodes) %>%
@@ -105,4 +127,9 @@ hoods %>%
                     name = 'group', 
                     guide = guide_discrete) +
   theme_bm_legend()
+
+##
+
+ggsave("regions.png", height = 6, width = 8, dpi = 300)
+
 
